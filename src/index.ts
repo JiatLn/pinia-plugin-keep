3610c1ef7331db69577af1d152e3e4acc4300d71
林佳涛 <lne128gene980@163.com>
2022-07-20 16:22:58 +0800

    feat: add resetPlugin to reset state with keypath

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export { keepPiniaPlugin } from './plugin'
+import 'pinia'
+
+export { keepPiniaPlugin, resetPluginPinia } from './plugin'
+
+declare module 'pinia' {
+  export interface PiniaCustomProperties {
+    /**
+     * Resets the store to its initial state with a nested value pass the keypath as the argument.
+     * [warn]: this fn now is options api only
+     */
+    $resetPath: (path: string) => void
+  }
+}
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,3 +1,4 @@
+import { get, has, set } from 'lodash-es'
 import type { PiniaPluginContext } from 'pinia'
 import { useStorage } from './composables/useStorage'
 
@@ -10,4 +11,19 @@ export function keepPiniaPlugin(context: PiniaPluginContext) {
 
   const init = getItem(store.$id) as any
   store.$patch({ ...init })
+}
+
+export function resetPluginPinia(context: PiniaPluginContext) {
+  const { store, options } = context
+
+  store.$resetPath = (path: string) => {
+    if (!store._isOptionsAPI)
+      throw new Error(`🍍: Store "${store.$id}" is built using the setup syntax and does not implement $resetPath().`)
+    const { state } = options
+    const newState = state ? state() : {}
+    if (!has(newState, path))
+      throw new Error(`🍍: Store "${store.$id}" could not resloved the path of '${path}'.`)
+    const resetVal = get(newState, path)
+    set(store.$state, path, resetVal)
+  }
 }
